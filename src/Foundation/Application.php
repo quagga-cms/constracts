2891277f61aad6b6b72a8402dfdadc4f1ad482f5
Puleeno Nguyen <puleeno@gmail.com>
2024-05-02 14:25:15 +0700

    Load configs

--- src/Foundation/Application.php
+++ src/Foundation/Application.php
@@ -2,31 +2,38 @@
 
 namespace Quagga\Contracts\Foundation;
 
+use Closure;
 use Illuminate\Contracts\Container\Container;
 
 interface Application extends Container
 {
-    /**
-     * Register a new "booted" listener.
-     *
-     * @param  callable  $callback
-     * @return void
-     */
-    public function booted($callback);
-
-    public function boot();
-
     public function registerBaseServiceProviders();
 
     public function registerCoreContainerAliases();
 
+    public function detectEnvironment(Closure $callback);
+
     public function registerConfiguredProviders();
+
+    public function getCachedServicesPath();
+
+    public function getCachedPackagesPath();
+
+    public function configurationIsCached();
+
+    public function getCachedConfigPath();
 
     public function bootstrapWith(array $bootstrappers);
 
     public function setBasePath(string $basePath);
 
+    public function path($path = '');
+
     public function basePath($path = '');
+
+    public function bootstrapPath($path = '');
+
+    public function configPath($path = '');
 
     public function register($provider, $force = false);
 
@@ -38,7 +45,17 @@ interface Application extends Container
 
     public function isBooted();
 
+    public function booted($callback);
+
+    public function boot();
+
     public function hasBeenBootstrapped();
 
     public function shouldSkipMiddleware();
+
+    public function getDeferredServices();
+
+    public function setDeferredServices(array $services);
+
+    public function addDeferredServices(array $services);
 }
